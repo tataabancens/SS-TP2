9b27620517e65df6191d893918e075a82a5f4ee0
Alberto <tataabancens@gmail.com>
2022-08-22 00:09:32 -0300

    Feat: Done

--- src/main/java/ar/edu/itba/ss/App.java
+++ src/main/java/ar/edu/itba/ss/App.java
@@ -1,5 +1,9 @@
 package ar.edu.itba.ss;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
 /**
  * Hello world!
  *
@@ -10,7 +14,25 @@ public class App
     {
         GameOfLife gof = new GameOfLife(10);
 
+        for (int i = 0; i < 10; i++) {
+            gof.next();
+        }
+        // Store results
+        String filePath = "maps.json"; // LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE)
+        App.writeToFile(filePath, gof.getMaps().toJSONString());
+    }
 
-        System.out.println( "Hello World!" );
+    private static void writeToFile(String filepath, String toWrite) {
+        try {
+            FileWriter fw = new FileWriter(filepath, true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter pw = new PrintWriter(bw);
+
+            pw.print(toWrite);
+            pw.flush();
+            pw.close();
+        } catch (Exception e) {
+            System.out.println("Failed");
+        }
     }
 }
--- src/main/java/ar/edu/itba/ss/GameOfLife.java
+++ src/main/java/ar/edu/itba/ss/GameOfLife.java
@@ -1,5 +1,8 @@
 package ar.edu.itba.ss;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -7,15 +10,18 @@ import java.util.Random;
 public class GameOfLife {
     private final List<Cell> cells;
     private final int M;
+    private int t;
+    private JSONArray maps = new JSONArray();
 
     public GameOfLife(int M) {
         this.cells = new ArrayList<>();
         this.M = M;
+        t = 0;
         // Initialize cells
         initializeCells();
     }
 
-    public void initializeCells() {
+    private void initializeCells() {
         Random r = new Random();
         for(int i = 0; i < getM(); i++) {
             for(int j = 0; j < getM(); j++) {
@@ -33,11 +39,47 @@ public class GameOfLife {
 
     public void next() {
         calculateNeighbours();
-
         updateMap();
+        addMapToJson();
+        t++;
     }
 
-    public void calculateNeighbours() {
+    public void addMapToJson() {
+        JSONObject jsMap = new JSONObject();
+        jsMap.put("t", getT());
+        JSONArray rows = new JSONArray();
+        for (int i = 0; i < getM(); i++) {
+            JSONArray col = new JSONArray();
+            for (int j = 0; j < getM(); j++) {
+                col.add(getCells().get(i + j * M).isAlive());
+            }
+            rows.add(col);
+        }
+        jsMap.put("map", rows);
+        maps.add(jsMap);
+    }
+
+    public JSONArray getMaps() {
+        return maps;
+    }
+
+    public void printMap() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < getM(); i++) {
+            for (int j = 0; j < getM(); j++) {
+                if (cells.get(i + j * getM()).isAlive()) {
+                    sb.append("*");
+                } else {
+                    sb.append(" ");
+                }
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+
+
+    private void calculateNeighbours() {
         for(Cell c : getCells()) {
             NeighbourCells nc = new NeighbourCells(c, M);
 
@@ -49,7 +91,7 @@ public class GameOfLife {
         }
     }
 
-    public void updateMap() {
+    private void updateMap() {
         for(Cell c : getCells()) {
             if (c.isAlive()) {
                 if (c.getNeighbours() < 2 || c.getNeighbours() > 3) {
@@ -58,6 +100,7 @@ public class GameOfLife {
             } else if (c.getNeighbours() == 3) {
                 c.setLife(true);
             }
+            c.resetNeighbours();
         }
     }
 
@@ -69,5 +112,7 @@ public class GameOfLife {
         return cells;
     }
 
-
+    public int getT() {
+        return t;
+    }
 }
