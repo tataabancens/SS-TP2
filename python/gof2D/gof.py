ceb36d357dfeb3cdeb5daa5b338cbd7979ca2e83
Alberto <tataabancens@gmail.com>
2022-09-02 09:55:46 -0300

    Last push

--- python/gof2D/gof.py
+++ python/gof2D/gof.py
@@ -2,8 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import matplotlib.animation as animation
 import pandas as pd
+import matplotlib as mpl 
 
-df = pd.read_json('results\\rule0\maps80.json')
+mpl.rcParams['animation.ffmpeg_path'] = r'C:\\Users\\tata_\SS\\ffmpeg-2022-08-31-git-319e8a49b5-essentials_build\bin\\ffmpeg.exe'
+
+rule = '2'
+
+df = pd.read_json('results\\rule' + rule + '\maps70.json')
 grid = df.iloc[0]['map']
 i = 0
 def update(data):
@@ -15,9 +20,12 @@ def update(data):
         return [mat]
 
 
-
 fig, ax = plt.subplots()
 mat = ax.matshow(grid)
-ani = animation.FuncAnimation(fig, update, interval=100,
+anim = animation.FuncAnimation(fig, update, interval=100,
                               save_count=5000)
-plt.show()
+# plt.show()
+
+f = r"C:\\Users\\tata_\SS\SS-TP2\\python\\gof2D\\animations\\gofRule2.mp4"
+writervideo = animation.FFMpegWriter(fps=12) 
+anim.save(f, writer=writervideo)
